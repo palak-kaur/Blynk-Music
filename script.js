45c3215dd81783c6f910ad376cebecf1df2ed4a0
Palak Kaur <palakkaur0308@gmail.com>
2021-05-31 17:36:52 +0530

    v4

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ $( document ).ready(function() {
    let redirect_uri = 'https%3A%2F%2Fpalak-kaur.github.io%2FBlynk-Music'; // GitHub Pages URL or whatever your public url to this app is
    // *************** END *************************
 
-//    const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
-//    // Don't authorize if we have an access token already
-//    if(accessToken == null || accessToken == "" || accessToken == undefined){
-//      window.location.replace(redirect);
-//    }
+   const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
+   // Don't authorize if we have an access token already
+   if(accessToken == null || accessToken == "" || accessToken == undefined){
+     window.location.replace(redirect);
+   }
 
 //    // Search button has been clicked
 //    $( "#search_button" ).click(function() {
@@ -79,8 +79,37 @@ $( document ).ready(function() {
             
          $(".search-bar").on('focus', function(){
             $(".search-bar").text("");
-            console.log("c");
-        });
+            //Get the value of the search box
+     let raw_search_query = $('#search-text').val();
+     let search_query = encodeURI(raw_search_query);
+     // Make Spotify API call
+     // Note: We are using the track API endpoint.
+     $.ajax({
+       url: `https://api.spotify.com/v1/search?q=${search_query}&type=track`,
+       type: 'GET',
+       headers: {
+           'Authorization' : 'Bearer ' + accessToken
+       },
+       success: function(data) {
+         // Load our songs from Spotify into our page
+         let num_of_tracks = data.tracks.items.length;
+         let count = 0;
+         // Max number of songs is 12
+         const max_songs = 12;
+         while(count < max_songs && count < num_of_tracks){
+           // Extract the id of the FIRST song from the data object
+           let id = data.tracks.items[count].id;
+           // Constructing two different iframes to embed the song
+           let src_str = `https://open.spotify.com/embed/track/${id}`;
+           let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+           let parent_div = $('#song_'+ count);
+           parent_div.html(iframe);
+           count++;
+         }
+       }
+     }); // End of Spotify ajax call
+   
+ });
         isSearchOpen = true;
         }
     });
