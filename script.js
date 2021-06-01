7852f2b53d8461c59712682cb23a9a94e7cfe436
Palak Kaur <palakkaur0308@gmail.com>
2021-06-01 16:04:23 +0530

    v5

--- script.js
+++ script.js
@@ -34,7 +34,35 @@ $( document ).ready(function() {
    }
 
 //HOME =================================================================================
+let category = ["taylor","motivational","bollywood","punjabi"];
+for(let i = 0 ; i < 4 ; i++){
+$.ajax({
+    url: `https://api.spotify.com/v1/search?q=${category[i]}&type=track`,
+    type: 'GET',
+    headers: {
+    'Authorization' : 'Bearer ' + accessToken
+},
+success: function(data) {
+// Load our songs from Spotify into our page
+let num_of_tracks = data.tracks.items.length;
+let count = 0;
 
+// Max number of songs is 12
+const max_songs = 5;
+while(count < max_songs && count < num_of_tracks){
+// Extract the id of the FIRST song from the data object
+let id = data.tracks.items[count].id;
+// Constructing two different iframes to embed the song
+let src_str = `https://open.spotify.com/embed/track/${id}`;
+let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+let parent_div = $('#song_'+ count);
+parent_div.html(iframe);
+
+count++;
+    }
+}
+}); // End of Spotify ajax call
+}
 
 
 
