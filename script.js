d41f26e5daff11ca77f538408016770d37a13ae3
Palak Kaur <palakkaur0308@gmail.com>
2021-06-01 15:55:53 +0530

    v5

--- script.js
+++ script.js
@@ -33,39 +33,12 @@ $( document ).ready(function() {
      window.location.replace(redirect);
    }
 
-//    // Search button has been clicked
-//    $( "#search_button" ).click(function() {
-//      //Get the value of the search box
-//      let raw_search_query = $('#search-text').val();
-//      let search_query = encodeURI(raw_search_query);
-//      // Make Spotify API call
-//      // Note: We are using the track API endpoint.
-//      $.ajax({
-//        url: `https://api.spotify.com/v1/search?q=${search_query}&type=track`,
-//        type: 'GET',
-//        headers: {
-//            'Authorization' : 'Bearer ' + accessToken
-//        },
-//        success: function(data) {
-//          // Load our songs from Spotify into our page
-//          let num_of_tracks = data.tracks.items.length;
-//          let count = 0;
-//          // Max number of songs is 12
-//          const max_songs = 12;
-//          while(count < max_songs && count < num_of_tracks){
-//            // Extract the id of the FIRST song from the data object
-//            let id = data.tracks.items[count].id;
-//            // Constructing two different iframes to embed the song
-//            let src_str = `https://open.spotify.com/embed/track/${id}`;
-//            let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
-//            let parent_div = $('#song_'+ count);
-//            parent_div.html(iframe);
-//            count++;
-//          }
-//        }
-//      }); // End of Spotify ajax call
-//    }); // End of search button
+//HOME =================================================================================
 
+
+
+
+//SEARCH ================================================================================
     let isSearchOpen = false;
     $(".search-nav").click(function(e){
         if(!isSearchOpen){
@@ -160,6 +133,8 @@ $(document).on('keydown', function(event) {
     }
 
 });
+
+
 
  }); // End of document.ready
 
