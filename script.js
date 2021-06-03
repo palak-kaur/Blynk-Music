f08ff0cbd3d4716210b3d00685637a368973e588
Palak Kaur <palakkaur0308@gmail.com>
2021-06-03 19:28:51 +0530

    v6

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
                     // Constructing two different iframes to embed the song
                     let src_str = `https://open.spotify.com/embed/track/${id}`;
                     let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
-                    let parent_div = $(`#row-${i} #row-home_${count}`);
+                    parent_div = document.getElementsById("row" + i).getElementsById("row-home_" + count);
                     parent_div.append(iframe);
 
                     count++;
